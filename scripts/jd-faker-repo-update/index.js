358783d401c19436b0da7235c2bc08f03274b4ea
yanl <ly@bubu100.com>
2021-10-12 16:05:38 +0800

    :bug: 调整命令执行方式

--- scripts/jd-faker-repo-update/index.js
+++ scripts/jd-faker-repo-update/index.js
@@ -15,12 +15,13 @@ request.get("https://api.github.com/repos/shufflewzc/faker2").then((res) => {
     console.log("当前项目代码五分钟之内进行过更新,将触发青龙更新");
 
     exec(
-      'docker exec -it qinglong ql repo https://ghproxy.com/https://github.com/shufflewzc/faker2.git "jd_|jx_|gua_|jddj_|getJDCookie" "activity|backUp" "^jd[^_]|USER|function|utils|ZooFaker_Necklace.js|JDJRValidator_Pure|sign_graphics_validate|ql"',
+      'docker exec -i qinglong ql repo https://ghproxy.com/https://github.com/shufflewzc/faker2.git "jd_|jx_|gua_|jddj_|getJDCookie" "activity|backUp" "^jd[^_]|USER|function|utils|ZooFaker_Necklace.js|JDJRValidator_Pure|sign_graphics_validate|ql"',
       function (err, stdout, stderr) {
         if (err) {
-          console.error("青龙更新失败");
+          console.error("青龙更新失败\n");
+          console.log("err");
         } else {
-          console.error("青龙更新成功");
+          console.log("青龙更新成功");
         }
       }
     );
