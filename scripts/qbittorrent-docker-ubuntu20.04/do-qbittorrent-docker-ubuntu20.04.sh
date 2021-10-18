d770964c01300a9746d4c6a12324cb9ee09ab97c
yanl <ly@bubu100.com>
2021-10-18 09:38:41 +0800

    :bug: 删除 cd 的 sudo :sparkles: 安装过程时添加安装几个包

--- scripts/qbittorrent-docker-ubuntu20.04/do-qbittorrent-docker-ubuntu20.04.sh
+++ scripts/qbittorrent-docker-ubuntu20.04/do-qbittorrent-docker-ubuntu20.04.sh
@@ -5,14 +5,16 @@ sudo apt-get update -y && apt-get upgrade -y
 
 # 清理 docker 历史包以及安装下面需要用到的工具包
 sudo apt-get remove --autoremove --purge docker docker-engine docker.io containerd runc -y
-sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release vim -y
+sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release vim git net-tools htop neofetch cpulimit -y
+sudo apt-get clean
 
 # 安装 docker 并且配置开机自启
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 sudo echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y && sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo systemctl start docker && systemctl enable docker
 
 # 安装 docker-compose
@@ -35,9 +37,9 @@ services:
       - /opt/qbittorrent/config:/config
       - /opt/qbittorrent/downloads:/downloads
     ports:
-      - 32671:32671
-      - 32671:32671/udp
+      - 6881:6881
+      - 6881:6881/udp
       - 8080:8080
     restart: unless-stopped
 EOF
-sudo cd /opt/qbittorrent && sudo docker-compose up -d
+cd /opt/qbittorrent && sudo docker-compose up -d
